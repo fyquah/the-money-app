fa8c56ccbb1c38156e3823dc791c4757b963dfc6
fyquah95 <quah.fy95@gmail.com>
2014-08-17 13:46:56 +0800

    dry-code initialized

--- app/assets/javascripts/accounting_transactions.js
+++ app/assets/javascripts/accounting_transactions.js
@@ -5,9 +5,12 @@ function create_content(type , properties){
   Mustache.parse(template);   // optional, speeds up future uses
   var rendered = Mustache.render(template , {
     record_type: (type + "_records"),
-    record_count: (type == "credit" ? credit_record_count : debit_record_count )
+    record_count: (properties.record_count),
+    record_id: (properties.record_id || ""),
+    account_name: (properties.account_name || ""),
+    account_type: (properties.account_type || ""),
+    amount: (properties.amount || "")
   })
-  type == "credit" ? credit_record_count++ : debit_record_count++
   if(type == "credit")
     $("#credit_records").append(rendered)
   else
@@ -15,6 +18,12 @@ function create_content(type , properties){
 }
   
 $(document).ready(function(){
-  $("#create_new_credit_entry").click(function(){ create_content("credit") })
-  $("#create_new_debit_entry").click(function(){ create_content("debit") })
+  $("#create_new_credit_entry").click(function(){ 
+    create_content("credit" , { record_count: credit_record_count })
+    credit_record_count++
+  })
+  $("#create_new_debit_entry").click(function(){ 
+    create_content("debit" , {  record_count: debit_record_count })
+    debit_record_count++ 
+  })
 })
